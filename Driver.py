1bb38dba062c5c1f50d7f2499fb91fc3d205c79f
Eric Chen <chen.ericsn@outlook.com>
2023-10-07 02:32:22 -0500

    Revert "Revert "[Step 1] Feat: Implemented PricingUtil Python module and unit tests""

--- Driver.py
+++ Driver.py
@@ -3,4 +3,11 @@ import PricingUtil
 
 
 if __name__ == "__main__":
+    prevPrice = 10.34
+    interest = 1.5
+    oleoConstant = 0.8
+    print(f"Previous price: {prevPrice}, interest: {interest}, Oleo Constant: {oleoConstant}")
+    priceUtil = PricingUtil.PricingUtil()
+    val = priceUtil.calcVal(prevPrice, interest, oleoConstant)
+    print(f"Theoretical value is: {val}")
     print("Nice work on creating the util! Make sure to check the unit tests!")
