830a51558181c46f1e4dc6d08b013bfdacbff3d3
Eric Chen <chen.ericsn@outlook.com>
2023-10-08 00:39:21 -0500

    Improved area finding algorithm from O(n^2) to O(n)

--- src/improved.py
+++ src/improved.py
@@ -1,13 +1,17 @@
 def max_area(height):
     max_area = 0
     n = len(height)
+    
+    left = 0
+    right = n-1
 
-    for i in range(n):
-        for j in range(i + 1, n):
-            h1 = height[i]
-            h2 = height[j]
-            width = j - i
-            current_area = min(h1, h2) * width
-            max_area = max(max_area, current_area)
-
+    while left < right:
+        area = (right - left) * min(height[left], height[right])
+        if area > max_area:
+            max_area = area
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+            
     return max_area
